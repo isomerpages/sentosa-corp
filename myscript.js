8ae8eba7f197850b61681d15472d5ccac184e31f
nicolema <ma-xue-fei@hotmail.com>
2020-08-06 19:28:28 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,3 +3,8 @@ script.type = 'text/javascript';
 script.src = 'https://assets.dcube.cloud/scripts/wogaa.js';    
 
 document.getElementsByTagName('head')[0].appendChild(script);
+
+if (document.getElementsByTagName("META")[4].content = "Environmental Reports")
+{
+  document.getElementsByClassName("is-8").style.width = "100%";
+}
